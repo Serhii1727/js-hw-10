e761c1984b79c99db588433647e6a0288a176998
Serhii1727 <kravchenko087@ukr.net>
2023-11-06 13:57:17 -0500

    add searchCat

---
+++ src/cat-api.js
@@ -0,0 +1,11 @@
+const API_KEY = 'live_DcDrCMTF2pSSJT9cxuG4ThEpzKaOOnLdMJ7gLIYPozlvvxoPgk2nyqUbCjfVtRYn';
+const BASE_URL = 'https://api.thecatapi.com/v1'
+export function fetchBreeds() {
+    return fetch("https://api.thecatapi.com/v1/breeds").then((response) => { return response.json() })
+}
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`).then((response) => { return response.json() })
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-const apiKey = live_DcDrCMTF2pSSJT9cxuG4ThEpzKaOOnLdMJ7gLIYPozlvvxoPgk2nyqUbCjfVtRYn;
+import SlimSelect from "slim-select";
+import Notiflix from "notiflix";
+import { fetchBreeds } from "./cat-api";
+import { fetchCatByBreed } from "./cat-api";
+
+const refs = {
+    body: document.querySelector('body'),
+    selectContainer: document.querySelector('.select-container'),
+    select: document.querySelector('.breed-select'),
+    divCatInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.container-loader')
+};
+
+fetchBreeds().then((breeds) => {
+    addValueToSelect(breeds);
+    refs.selectContainer.classList.remove('is-hidden');
+    refs.loader.classList.add('is-hidden')
+}).catch(onFetchErorr);
+
+function addValueToSelect(breeds) {
+    
+
+    const markup = breeds.map(({ id, name }) => {
+        return `<option value='${id}'>${name}</option>`
+    }).join('');
+
+    refs.select.insertAdjacentHTML('beforeend', markup);
+
+    new SlimSelect({
+        select: document.querySelector('.breed-select'),
+    });
+}
+
+function renderCatByBread(event) {
+    clearCardOfCat()
+    const breedId = event.target.value;
+    fetchCatByBreed(breedId).then((data) => {
+        refs.loader.classList.remove('is-hidden')
+        makeCardOfCat(data)
+        refs.loader.classList.add('is-hidden')
+
+    }).catch(onFetchErorr);
+}
+
+function onFetchErorr(error) {
+    console.log(error);
+    Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+}
+
+function makeCardOfCat(data) {
+
+    const cat = data[0];
+
+    const { url, breeds: [{ name, description, temperament }] } = cat;
+
+    const markupCat = `<div>
+                        <img src="${url}" alt="${name}" width="750" height="550">
+                        <h1>${name}</h1>
+                        <p>${description}</p>
+                        <p>Temperament: ${temperament}</p>
+                    </div>`;
+    
+    refs.divCatInfo.insertAdjacentHTML('beforeend', markupCat);
+}
+
+function clearCardOfCat() {
+    refs.divCatInfo.innerHTML = ''
+}
+
+const breedId = refs.select.addEventListener('change', renderCatByBread);
